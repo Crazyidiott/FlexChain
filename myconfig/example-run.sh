5411a7d0121d7734908d9d9a6089181d7c1c1d83
Sicong <liusicong1998@outlook.com>
2024-09-17 03:35:01 -0600

    Add single node test config

---
+++ myconfig/example-run.sh
@@ -0,0 +1,5 @@
+./storage_server -a 0.0.0.0:50051 -d ../mydata/testdb
+./memory_server -c ../myconfig/memory.config
+./orderer -l -a 0.0.0.0:50053 -c ../myconfig/consensus.config
+./compute_server -a 0.0.0.0:50054 -c ../myconfig/compute.config
+./compute_server -v -a 0.0.0.0:50055 -c ../myconfig/compute.config
