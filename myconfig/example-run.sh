132e59dd0cc3c68ac8516f9853ba44351bde5406
Crazyidiott <1073405983@qq.com>
2025-03-07 16:20:03 +0800

    updateConfig

--- myconfig/example-run.sh
+++ myconfig/example-run.sh
@@ -1,5 +1,5 @@
 sudo ./storage_server -a 0.0.0.0:50051 -d ../mydata/testdb
 sudo ./memory_server -c ../myconfig/memory.config
-sudo ./orderer -l -a 0.0.0.0:50051 -c ../myconfig/consensus.config
-sudo ./orderer -f -a 0.0.0.0:50051 -c ../myconfig/consensus.config
+sudo ./orderer -l -a 0.0.0.0:50053 -c ../myconfig/consensus.config
+sudo ./orderer -f -a 0.0.0.0:50053 -c ../myconfig/consensus.config
 sudo ./compute_server -v -a 0.0.0.0:50051 -c ../myconfig/compute.config
