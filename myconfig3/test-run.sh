a5b50b7c25bb2991c99427686700802a9ba8d341
Crazyidiott <1073405983@qq.com>
2025-03-09 16:26:58 +0800

    updateConfig

---
+++ myconfig3/test-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start storage server (in background)
+./storage_server -a 127.0.0.1:50051 -d ./mydata/testdb &
+sleep 2
+
+# Start memory server (in background)
+./memory_server -c ./myconfig/memory.config &
+sleep 2
+
+# Start orderer as leader (in background)
+./orderer -l -a 127.0.0.1:50054 -c ./myconfig/consensus.config &
+sleep 2
+
+# Start compute server as validator
+./compute_server -v -a 127.0.0.1:50053 -c ./myconfig/compute.config
+
+# Wait for all background processes
+wait
---
+++ myconfig3/test.sh
@@ -0,0 +1,9 @@
+sudo ./storage_server -a 0.0.0.0:50051 -d ../mydata/testdb
+sudo ./memory_server -c ../myconfig/memory.config
+sudo ./orderer -l -a 0.0.0.0:50054 -c ../myconfig/consensus.config
+sudo ./orderer -f -a 0.0.0.0:50055 -c ../myconfig/consensus.config
+sudo ./orderer -f -a 0.0.0.0:50056 -c ../myconfig/consensus.config
+sudo ./compute_server -v -a 0.0.0.0:50053 -c ../myconfig/compute.config
+
+cd ~/FlexChain/ && git pull
+cp ./myconfig/* /opt/Sicong/FlexChain/myconfig/ && cd /opt/Sicong/FlexChain/build
