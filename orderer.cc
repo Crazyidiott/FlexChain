146aaaa55f5c79815916f34e021274e4912cebe5
Crazyidiott <1073405983@qq.com>
2025-03-24 10:33:21 +0800

    updateOrderFstream

--- orderer.cc
+++ orderer.cc
@@ -415,7 +415,7 @@ void run_follower(const std::string &server_address) {
     std::filesystem::create_directory("./consensus");
 
     //TODO why follower doesn't need to open the log file?
-    global_log_file.open("./consensus/raft.log", ios::in | ios::out | ios::binary | ios::trunc);
+    global_log_file.open("./consensus/raft.log", ios::in | ios::out | ios::binary);
     assert(global_log_file.is_open());
 
     /* start the grpc server for ConsensusComm */
