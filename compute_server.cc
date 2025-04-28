ae37f75156ddce6f38703549424aae092489169f
Crazyidiott <1073405983@qq.com>
2025-04-28 11:00:08 +0800

    two simluation handler test

--- compute_server.cc
+++ compute_server.cc
@@ -1380,7 +1380,7 @@ void run_server(const string &server_address, bool is_validator) {
     int num_threads = c_config_info.num_qps_per_server;
     int num_sim_threads = c_config_info.num_sim_threads;
     //TODO: HARD CODED
-    CoreManager core_manager(1, 1, num_threads);
+    CoreManager core_manager(2, 1, num_threads);
     std::vector<int> specific_cores = {0}; 
     core_manager.initialize(1, specific_cores);
     // #region original initialization code
@@ -1449,7 +1449,6 @@ void run_server(const string &server_address, bool is_validator) {
 
     //=================threads number adjustment=============================
     sleep(10);
-    // core_manager.add_core(8);
     core_manager.adjust_thread(-1, 0);
     sleep(10);
     core_manager.adjust_thread(1, 0);
