e6090172e9fcbdcf7ad88daecdc6ace069536c24
Crazyidiott <1073405983@qq.com>
2025-04-28 11:19:58 +0800

    kmeans test

--- compute_server.cc
+++ compute_server.cc
@@ -1449,9 +1449,9 @@ void run_server(const string &server_address, bool is_validator) {
 
     //=================threads number adjustment=============================
     sleep(10);
-    core_manager.adjust_thread(-1, 0);
+    // core_manager.adjust_thread(-1, 0);
     sleep(10);
-    core_manager.adjust_thread(1, 0);
+    // core_manager.adjust_thread(1, 0);
 
     // core_manager.remove_core(0);
     //=======================================================================
