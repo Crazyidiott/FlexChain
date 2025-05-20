9352493bef3131d2d9b874ff9bc0376bb5637845
Crazyidiott <1073405983@qq.com>
2025-05-21 00:30:17 +0800

    update benchmark

--- benchmark.cc
+++ benchmark.cc
@@ -310,15 +310,15 @@ void *client_thread(void *arg) {
             type = (type + 1) % 3;
             if(type == 0){
                 log_info(stderr, "workload type is YCSB");
-                trans_per_interval = 100; //2000/s
+                trans_per_interval = 25; //2000/s
             }
             else if(type == 1){
                 log_info(stderr, "workload type is KMEANS");
-                trans_per_interval = 100; //2000/s
+                trans_per_interval = 25; //2000/s
             }
             else if(type == 2){
                 log_info(stderr, "workload type is SMALLBANK");
-                trans_per_interval = 100; //2000/s
+                trans_per_interval = 25; //2000/s
             }
         }
         
